cfff7635a657190c0378340700f22c9615398961
pankaj9695 <pankaj9695@yahoo.com>
2023-07-20 19:11:53 +0530

    amplify domain added

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: [
       "fabweltmarketplace.s3.us-east-2.amazonaws.com",
       "raw.githubusercontent.com",
+      "d15x3nwgjcy1f5.cloudfront.net",
     ],
   },
 };
